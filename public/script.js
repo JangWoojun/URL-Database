5d5a89d91d969cefd2139c291d89a5b11678ff45
JangWoojun <woojun0107@naver.com>
2024-01-21 09:47:23 +0900

    필드 명 변경

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 function saveUserInfo() {
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
     // URL에 정보 추가
-    var url = window.location.href.split('?')[0] + `?name=${name}&age=${age}`;
+    var url = window.location.href.split('?')[0] + `?username=${username}&password=${password}`;
 
     // 정보 표시
-    document.getElementById("displayUserInfo").innerHTML = `<p>이름: ${name}</p><p>나이: ${age}</p>`;
+    document.getElementById("displayUserInfo").innerHTML = `<p>아이디: ${username}</p><p>비밀번호: ${password}</p>`;
 
     // URL 갱신
     window.history.replaceState({}, document.title, url);
@@ -15,12 +15,12 @@ function saveUserInfo() {
 // 페이지 로드 시 URL에서 정보 읽어오기
 window.onload = function() {
     var params = new URLSearchParams(window.location.search);
-    var name = params.get('name');
-    var age = params.get('age');
+    var username = params.get('username');
+    var password = params.get('password');
 
-    if (name && age) {
-        document.getElementById("name").value = name;
-        document.getElementById("age").value = age;
-        document.getElementById("displayUserInfo").innerHTML = `<p>이름: ${name}</p><p>나이: ${age}</p>`;
+    if (username && password) {
+        document.getElementById("username").value = username;
+        document.getElementById("password").value = password;
+        document.getElementById("displayUserInfo").innerHTML = `<p>아이디: ${username}</p><p>비밀번호: ${password}</p>`;
     }
 };
